eb66f5461f8ec4b25d9c83d7563b2253ccfbf5ad
Nico Smith <nicosmith873@gmail.com>
2023-12-16 14:55:52 -0500

    Added Nico

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Nicos Website!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Nicos app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Nicos website is cool!
         </p>
       </main>
 
