bc3645df6cfeb160d8618cf6224e09f465d32349
niiccoo2 <101956935+niiccoo2@users.noreply.github.com>
2023-12-18 13:02:35 +0000

    Fixed Error

--- pages/storys.js
+++ pages/storys.js
@@ -42,19 +42,5 @@ export default function Home() {
 
        <Footer />
     </div>
-    <form name="contact" method="POST" netlify>
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
   )
 }
