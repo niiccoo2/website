bd32725435596b01268dc435cf48b0d3449ff52c
niiccoo2 <101956935+niiccoo2@users.noreply.github.com>
2024-01-04 18:50:47 +0000

    Added made with next.js and notes.md

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,8 @@ export default function Footer() {
     <footer className={styles.footer}>
       <p style={{ textAlign: 'center' }}>
         <a href="https://nic0.win">Home</a><br />
-        Contact:&nbsp;<a href="mailto:contact@nic0.win">contact@nic0.win</a>
+        Contact:&nbsp;<a href="mailto:contact@nic0.win">contact@nic0.win</a>.<br></br><br></br>
+        Made with <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">next.js</a>.
       </p>
     </footer>
   );
---
+++ notes.md
@@ -0,0 +1,4 @@
+| | Add "Made with next.js" to footer <br>
+| | Add colors to examples <br>
+| | <br>
+| | <br>
