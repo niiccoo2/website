c81cafe6a05bac7bf251b4244b2b5c5e383298fe
niiccoo2 <nicosmith873@gmail.com>
2025-07-24 11:52:17 -0400

    adde we the people

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg-blue-600 text-white px-4 md:px-8 min-h-screen flex flex-col">
+    <div class="flex-1">
+        <h1 class="text-center text-2xl mt-10">Hello, I'm Nico!</h1>
+
+        <div class="flex justify-center p-8 gap-x-8">
+            <button class="bg-white text-blue-600 font-bold p-4 rounded hover:scale-105">
+                <a href="https://github.com/niiccoo2/">Github</a>
+            </button>
+            <button class="bg-white text-blue-600 font-bold p-4 rounded hover:scale-105">
+                <a href="https://www.qrz.com/db/KC1VPU">QRZ</a>
+            </button>
+        </div>
+    </div>
+    <footer class="text-center p-10 flex justify-center bg-blue-600 text-white">
+        Made with <span class="hover:animate-bounce">❤️</span> by Nico
+    </footer>
+</div>
---
+++ src/routes/we-the-people-unite/+page.svelte
@@ -0,0 +1,31 @@
+<div class="bg-blue-600 text-white px-4 md:px-10 min-h-screen flex flex-col">
+    <div class="flex-1">
+        <h1 class="text-center text-2xl mt-10">We The People Unite</h1>
+        <br>
+        <p class="text-left">In times of challenge, it is not our differences that define us, but our shared commitment to one another. No matter our background, beliefs, or who we support, we are strongest when we come together for the common good. Unity, compassion, and cooperation are the foundations of a better future.</p>
+        <br>
+        <p class=" text-left">This took around 15 hours to make. I made the whole design from scratch using Fusion 360 for 3d modeling, and a Prusa Mini+ for printing. To build it, I used a Pi Pico as the micro controller and 35 LED's. Due to the high current draw of the LED's I used 2N3904 transistors to control the set of LED's for each letter.</p>
+
+        <div class="flex justify-center p-8 gap-x-8">
+            <button class="bg-white text-blue-600 font-bold p-4 rounded hover:scale-105">
+                <a href="https://github.com/niiccoo2/We-The-People/">Github (Code)</a>
+            </button>
+            <button class="bg-white text-blue-600 font-bold p-4 rounded hover:scale-105">
+                <a href="https://photos.niiccoo2.xyz/share/ESwZD4Z7EnCDFlKrhO-xNTaDU5Q1B48rzCu0KGxKcLCwNLGHf0mgPwmt5CE1P3htN4c">More Photos</a>
+            </button>
+        </div>
+        <div class="flex justify-center p-8 gap-x-8">
+            <img class="w-70 rounded" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/1763638146ded1aa85799227576aa97c0691d9c3_pxl_20250615_220315571.mp.jpg" alt="We the People Project">
+            <img class="w-70 rounded" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/5a278b7c9dd013dc4df6f7595899a2962ea0cca3_pxl_20250615_135619842.mp.jpg" alt="We the People Project">
+            <img class="w-70 rounded" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/ab65e729ee2cb44814749bcf6a29c929fa1d9abd_pxl_20250616_014057597.mp.jpg" alt="We the People Project">
+        </div>
+        <div class="flex justify-center p-8 gap-x-8">
+            <img class="w-70 rounded" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/37017da7b5616e0bef19a4bfa090a3b604dc4938_pxl_20250616_042657979.mp.jpg" alt="We the People Project">
+            <img class="w-70 rounded" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/703829491d765d5f6669b95b6cb4ad4a3f30f842_pxl_20250615_193455802.jpg" alt="We the People Project">
+            <img class="w-70 rounded" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/42e36a493aedc5415bf58fcc8fd227c5e8d79b46_pxl_20250616_052529928.jpg" alt="We the People Project">
+        </div>
+    </div>
+    <footer class="text-center p-10 flex justify-center bg-blue-600 text-white">
+        Made with <span class="hover:animate-bounce">❤️</span> by Nico
+    </footer>
+</div>
