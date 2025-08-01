e92564bf779239c17d762abaea7ff9204fc3828f
Nico Smith <web@niiccoo2.xyz>
2025-07-31 21:46:37 -0400

    added new pictures

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -5,11 +5,16 @@
     <div class="flex-1">
         <div class="flex justify-center p-8 gap-x-8 sm:gap-x-2 md:gap-x-4 lg:gap-x-8 xl:gap-x-16">
             <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/IMGP6553.JPG" alt="Air Force Jet">
-            <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/PXL_20250220_201349368.jpg" alt="Sunset Skiing">
+            <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/_IGP7337.jpg" alt="Kids at the ocean">
         </div>
         <div class="flex justify-center p-8 gap-x-8 sm:gap-x-2 md:gap-x-4 lg:gap-x-8 xl:gap-x-16">
             <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/IMG_20250721_134611.jpg" alt="Person Walking infront of island">
-            <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/IMG_20230101_145802013_HDR-01.jpeg" alt="Lifeguard chairs">
+            <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/_IGP7349.jpg" alt="Cadillac Sunrise">
+        </div>
+        <div class="flex justify-center p-8 gap-x-8 sm:gap-x-2 md:gap-x-4 lg:gap-x-8 xl:gap-x-16.5"> <!-- Changed xl:gap-x to 16.5 from 16 so that the row of three matches the others -->
+            <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/_IGP7301.jpg" alt="Furn glowing in sun">
+            <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/_IGP7272.jpg" alt="Cars in Bath">
+            <img class="h-48 sm:h-64 md:h-80 lg:h-96 w-auto max-w-full rounded object-contain" src="/photos/_IGP7327.jpg" alt="Boy on Rocks">
         </div>
     </div>
     <footer class="text-center p-10 flex justify-center bg-blue-600 text-white">
