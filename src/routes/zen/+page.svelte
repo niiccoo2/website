6e5afa4b3ee1e20de8ecb8d5b301946cb44c481e
Nico Smith <web@niiccoo2.xyz>
2025-08-07 18:42:21 -0400

    added zen

---
+++ src/routes/zen/+page.svelte
@@ -0,0 +1,13 @@
+<div class="bg-blue-600 text-white px-4 md:px-10 min-h-screen flex flex-col">
+    <header class="text-left mt-10 mb-5 flex justify-left bg-blue-600 text-white">
+        <a class="text-2xl" style="font-family: 'Bitcount Single', monospace;" href="/">niiccoo2.xyz</a>
+    </header>
+    <p>I know there is nothing here... <br>
+        If it is past 08-17-2025 email me! <br>
+        you-forgot-the-website (at) niiccoo2.xyz <br>
+        Thank you!
+    </p>
+    <footer class="text-center p-10 flex justify-center bg-blue-600 text-white">
+        Made with <span class="hover:animate-bounce">❤️</span> by Nico
+    </footer>
+</div>
